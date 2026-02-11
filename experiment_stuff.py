4fcd026296a4bc9b130a2b6ba7bc4ae466f62ebb
Tomas <tomasly@chalmers.se>
2026-02-11 15:51:15 +0100

    did some things

--- experiment_stuff.py
+++ experiment_stuff.py
@@ -1,21 +1,21 @@
 from view import *
 
-n = 1000
+n = 20
 particles = []
 for i in range(n):
     theta = i*2*pi/n
     u = Vec(cos(theta),sin(theta))
-    pos = 8 * u
-    vel = -5 * u 
+    pos = 10 * u
+    vel = -1 * u 
     particles.append(Particle(1,pos,vel,0.6))
 
 def combined_walls(dt, particles):
-    wall_force(dt, particles, k=10, n=Vec(1, 0), a=Vec(-4,0))
-    wall_force(dt, particles, k=10, n=Vec(0, 1), a=Vec(0,-4))
-    wall_force(dt, particles, k=10, n=Vec(-1, 0), a=Vec(4,0))
-    wall_force(dt, particles, k=10, n=Vec(0, -1), a=Vec(0,4))
+    wall_force(dt, particles, k=1000000, n=Vec(1, 0), a=Vec(-10,0))
+    wall_force(dt, particles, k=1000000, n=Vec(0, 1), a=Vec(0,-10))
+    wall_force(dt, particles, k=1000000, n=Vec(-1, 0), a=Vec(10,0))
+    wall_force(dt, particles, k=1000000, n=Vec(0, -1), a=Vec(0,10))
 
-def combined_forces(dt, particles, forces=[combined_walls]):
+def combined_forces(dt, particles, forces=[combined_walls, constant_gravitational_field, collision]):
     for f in forces:
         if f is spring_force:
             f(dt, springs)
@@ -23,4 +23,4 @@ def combined_forces(dt, particles, forces=[combined_walls]):
         f(dt, particles)
     
 
-simulation_loop(combined_forces, 0.01, particles)
+simulation_loop(combined_forces, 0.0005, particles)
